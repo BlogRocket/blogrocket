0a44dd4dad8e476b014cd27cbeda51adbc8452e6
odelolajosh <oladapoodelola@gmail.com>
2023-03-13 16:15:52 +0100

    fix: fix error

--- apps/backend/src/middlewares/error.ts
+++ apps/backend/src/middlewares/error.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import AppError from '../utils/appError';
 
-const errorMiddleware = (err: any, _req: Request, res: Response) => {
+const errorMiddleware = (err: any, _req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       status: 'error',
