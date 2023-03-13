48281c1e81dbd1edbe600184d40fb1c28b4f5b7a
odelolajosh <oladapoodelola@gmail.com>
2023-03-13 12:48:33 +0100

    feat: User model

---
+++ apps/backend/custom.d.ts
@@ -0,0 +1,10 @@
+import type { IUser } from "./models/User";
+
+declare global {
+  namespace Express {
+    export interface Request {
+      token: string;
+      user: IUser
+    }
+  }
+}
---
+++ apps/backend/models/User.ts
@@ -0,0 +1,36 @@
+import { Schema, model, Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+export interface IUser {
+  email: string;
+  username: string;
+  password: string;
+}
+
+interface InstanceMethods {
+  comparePassword: (password: string) => Promise<boolean>;
+}
+
+export interface IUserModel extends Model<IUser, {}, InstanceMethods> {
+  // Add your static methods here
+}
+
+const UserSchema = new Schema<IUser, IUserModel, InstanceMethods>({
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+}, { timestamps: true });
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model<IUser, IUserModel>('User', UserSchema);
+export default User;
