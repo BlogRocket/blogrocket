6626c6c1e233251f05ed6035ffd1af2d5d69b9cc
odelolajosh <oladapoodelola@gmail.com>
2023-03-31 08:11:37 +0100

    fix: minor states

--- apps/frontend/src/features/post/components/PostHeader.tsx
+++ apps/frontend/src/features/post/components/PostHeader.tsx
@@ -12,7 +12,7 @@ export const PostHeader = ({ title = "", onSave }: PostHeaderProps) => {
   const navigate = useNavigate();
 
   return (
-    <header className="flex items-center gap-4 my-8 w-[600px] max-w-full">
+    <header className="flex items-center gap-4 py-8 w-[600px] max-w-full">
       <ArrowLeftIcon className="w-5 h-5 text-gray-400 mr-2 cursor-pointer" onClick={() => navigate(-1)} />
       <h1 className="text-xl font-bold">
         {title}
--- apps/frontend/src/features/post/routes/NewPost.tsx
+++ apps/frontend/src/features/post/routes/NewPost.tsx
@@ -26,7 +26,7 @@ export const NewPost: React.FC<NewPostProps> = () => {
       </div>
       {/* Overlay */}
       {createPost.isLoading && (
-        <div className="absolute inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
           <Spinner light />
         </div>
       )}
